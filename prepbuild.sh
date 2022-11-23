ea2710c969f45c00bda5cada7d010fde0947e97b
Marco Ridoni <m.ridoni@gmail.com>
2022-11-23 14:38:25 +0100

    Added new packaging scripts

---
+++ prepbuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# If /E is specified on the command line we take the following variables from the current environment
+if [ "$1" != "/E" ] ; then
+
+	# Check these variables and in case adjust them depending on your environment
+	HOST_PLATFORM=x64
+	# Check these variables (end)
+	
+	# These indicates the current version of GixSQL included in Gix-IDE
+	GIXSQLMAJ=1
+	GIXSQLMIN=0
+	GIXSQLREL=19dev1
+	# These indicates the current version of GixSQL included in Gix-IDE (end)
+
+fi
+
+echo "Configuring version ($GIXSQLMAJ.$GIXSQLMIN.$GIXSQLREL) in header file for GixSQL"
+echo "#define VERSION \"$GIXSQLMAJ.$GIXSQLMIN.$GIXSQLREL\"" > $SCRIPT_DIR/gixsql/config.h
