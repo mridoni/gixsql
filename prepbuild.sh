078cee43f8c057c8f567847e22b2ecefa4947f74
Marco Ridoni <m.ridoni@gmail.com>
2022-11-26 12:50:49 +0100

    Updated version to 1.0.19 Fixed Ubuntu packaging scripts

--- prepbuild.sh
+++ prepbuild.sh
@@ -12,7 +12,7 @@ if [ "$1" != "/E" ] ; then
 	# These indicates the current version of GixSQL included in Gix-IDE
 	GIXSQLMAJ=1
 	GIXSQLMIN=0
-	GIXSQLREL=19dev1
+	GIXSQLREL=19
 	# These indicates the current version of GixSQL included in Gix-IDE (end)
 
 fi
